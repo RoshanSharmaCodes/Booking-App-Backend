1b82038024101091a9819007c084ec17a685760f
Roshan Kumar Sharma <roshan.sharma@deprontoinfotech.com>
2023-03-19 05:55:38 +0530

    * Connected to MongoDB.

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-const express = require('express');
-const app = express();
+const express = require("express")
+const app = express()
+require("dotenv").config()
+const mongoose = require("mongoose")
 
-app.get("/",(req,res)=>{
-    res.json({success:true})
+const connectMongoDB = async () => {
+  try {
+    mongoose.connect(process.env.MONGODB_LINK)
+    console.log("Connected to MongoDB")
+  } catch (err) {
+    console.log("Failed To Connect to MongoDB",err)
+  }
+}
+
+mongoose.connection.on("disconnected", () => {
+    console.log("Connection Disconnected")
 })
 
-app.listen(8000,()=>{
-    console.log("Listening to port 8000")
+mongoose.connection.on("connected", () => {
+    console.log("Connection Completed")
+})
+
+app.get("/", (req, res) => {
+  res.json({ success: true })
+})
+
+app.listen(8000, () => {
+  console.log("Listening to port 8000")
+  connectMongoDB()
 })
